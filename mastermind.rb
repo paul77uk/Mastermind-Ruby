baa7cc21fe3fbf5629252151f0d41744e222f83d
Paul Vickers <paul77uk@gmail.com>
2022-10-12 15:05:16 +0100

    color rows updating

--- mastermind.rb
+++ mastermind.rb
@@ -30,41 +30,26 @@ class MasterMind
   def initialize
     @colors = [
       "\u25CB", YELLOW_PEG, WHITE_PEG, RED_PEG, ORANGE_PEG, GREEN_PEG, BROWN_PEG, BLUE_PEG, BLACK_PEG
-    ] # {color('black')}]
+    ]
     @guessing_colors_arr = ["\u25CB " * 4] * 10
     @input = nil
     @correct_guess_arr = ["\u2715 " * 4] * 10
     @index_number = 0
   end
 
-  def display # rubocop:disable Metrics/MethodLength
+  def display
     "     1 2 3 4 5 6 7 8\     \n "\
     "    #{YELLOW_PEG} #{WHITE_PEG} #{RED_PEG} #{ORANGE_PEG} #{GREEN_PEG} #{BROWN_PEG} #{BLUE_PEG} #{BLACK_PEG}\n\n" \
-    "10  #{"\u25CB " * 4}  #{@correct_guess_arr[9]}\n" \
-    "9   #{"\u25CB " * 4}  #{@correct_guess_arr[8]}\n" \
-    "8   #{"\u25CB " * 4}  #{@correct_guess_arr[7]}\n" \
-    "7   #{"\u25CB " * 4}  #{@correct_guess_arr[6]}\n" \
-    "6   #{"\u25CB " * 4}  #{@correct_guess_arr[5]}\n" \
-    "5   #{"\u25CB " * 4}  #{@correct_guess_arr[4]}\n" \
-    "4   #{"\u25CB " * 4}  #{@correct_guess_arr[3]}\n" \
-    "3   #{"\u25CB " * 4}  #{@correct_guess_arr[2]}\n" \
-    "2   #{"\u25CB " * 4}  #{@correct_guess_arr[1]}\n" \
+    "10  #{@guessing_colors_arr[9]}  #{@correct_guess_arr[9]}\n" \
+    "9   #{@guessing_colors_arr[8]}  #{@correct_guess_arr[8]}\n" \
+    "8   #{@guessing_colors_arr[7]}  #{@correct_guess_arr[7]}\n" \
+    "7   #{@guessing_colors_arr[6]}  #{@correct_guess_arr[6]}\n" \
+    "6   #{@guessing_colors_arr[5]}  #{@correct_guess_arr[5]}\n" \
+    "5   #{@guessing_colors_arr[4]}  #{@correct_guess_arr[4]}\n" \
+    "4   #{@guessing_colors_arr[3]}  #{@correct_guess_arr[3]}\n" \
+    "3   #{@guessing_colors_arr[2]}  #{@correct_guess_arr[2]}\n" \
+    "2   #{@guessing_colors_arr[1]}  #{@correct_guess_arr[1]}\n" \
     "1   #{@guessing_colors_arr[0]}  #{@correct_guess_arr[0]}"
-    # "1   #{"\u25CB " * 4}  #{color('correct')} \u2714 \u2715 \u2715"
-  end
-
-  def color(color)
-    str = "\u25CF"
-    correct = "\u2714"
-    return "\e[38;2;0;128;0m#{correct}\e[0m" if color == 'correct'
-    return "\e[38;2;220;20;60m#{str}\e[0m" if color == 'red'
-    return "\e[38;2;30;144;255m#{str}\e[0m" if color == 'blue'
-    return "\e[38;2;255;255;0m#{str}\e[0m" if color == 'yellow'
-    return "\e[38;2;255;165;0m#{str}\e[0m" if color == 'orange'
-    return "\e[38;2;0;255;0m#{str}\e[0m" if color == 'green'
-    return "\e[38;2;139;69;19m#{str}\e[0m" if color == 'brown'
-    return "\e[38;2;255;255;255m#{str}\e[0m" if color == 'white'
-    return "\e[38;2;0;0;0m#{str}\e[0m" if color == 'black'
   end
 
   def computer_colors
@@ -82,29 +67,32 @@ class MasterMind
 
   def update_guess_color_row
     guessing_line = @input.split('').map { |num| "#{@colors[num.to_i]} " }
-    @guessing_colors_arr[0] = guessing_line.join
+    @guessing_colors_arr[@index_number] = guessing_line.join
   end
+
+  # def win?
+  #   puts "You Won"
+  #   @guessing_colors_arr[@index_number] == computer_colors.join(" ")
+  # end
 
   def update_correct_guesses
     correct_peg_position_arr = []
     correct_peg_arr = []
     incorrect_peg_arr = []
-    # @correct_guess_arr.length.times do |number|
-      computer_colors.each_with_index do |color, index|
-        if color == @input[index]
-          correct_peg_position_arr << "#{CORRECT_PEG_POSITION} "
-        elsif @input.split('').include?(color)
-          correct_peg_arr << "#{CORRECT_PEG} "
-        else
-          incorrect_peg_arr << "#{INCORRECT_PEG} "
-        end
+    computer_colors.each_with_index do |color, index|
+      if color == @input[index]
+        correct_peg_position_arr << "#{CORRECT_PEG_POSITION} "
+      elsif @input.split('').include?(color)
+        correct_peg_arr << "#{CORRECT_PEG} "
+      else
+        incorrect_peg_arr << "#{INCORRECT_PEG} "
       end
-      @correct_guess_arr[@index_number] = correct_peg_position_arr.join + correct_peg_arr.join + incorrect_peg_arr.join
-      correct_peg_position_arr.clear
-      correct_peg_arr.clear
-      incorrect_peg_arr.clear
-      @index_number += 1
-    # end
+    end
+    @correct_guess_arr[@index_number] = correct_peg_position_arr.join + correct_peg_arr.join + incorrect_peg_arr.join
+    correct_peg_position_arr.clear
+    correct_peg_arr.clear
+    incorrect_peg_arr.clear
+    @index_number += 1
   end
 end
 
@@ -113,12 +101,12 @@ mastermind = MasterMind.new
 puts mastermind.display
 10.times do
   puts
-  # mastermind.display
-  # puts
   mastermind.enter_colors_guess
   puts mastermind.guess_correct?
   mastermind.update_guess_color_row
   mastermind.update_correct_guesses
   system 'clear'
   puts mastermind.display
+  puts
+  # return if mastermind.win?
 end
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,2 +1,13 @@
-else
-        #   INCORRECT_PEG
+def color(color)
+  #   str = "\u25CF"
+  #   correct = "\u2714"
+  #   return "\e[38;2;0;128;0m#{correct}\e[0m" if color == 'correct'
+  #   return "\e[38;2;220;20;60m#{str}\e[0m" if color == 'red'
+  #   return "\e[38;2;30;144;255m#{str}\e[0m" if color == 'blue'
+  #   return "\e[38;2;255;255;0m#{str}\e[0m" if color == 'yellow'
+  #   return "\e[38;2;255;165;0m#{str}\e[0m" if color == 'orange'
+  #   return "\e[38;2;0;255;0m#{str}\e[0m" if color == 'green'
+  #   return "\e[38;2;139;69;19m#{str}\e[0m" if color == 'brown'
+  #   return "\e[38;2;255;255;255m#{str}\e[0m" if color == 'white'
+  #   return "\e[38;2;0;0;0m#{str}\e[0m" if color == 'black'
+  # end
